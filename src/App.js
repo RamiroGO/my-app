df1ff416eb360a7f389ae702efff2ee1d4ba6f3f
Ramiro Javier Galindo Orozco <ramigao90@outlook.com>
2021-09-05 19:45:46 -0500

    Tabla con Botones
    Uso de React para generar una tabla que posee botones en cada celda y que son capaces de cambiar de un estado a otro haciendo uso de los setStated.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+// Código JSX
+
+// Componentes como Funciones
+// function HelloWorld(props) {
+//   console.log(props);
+//   return (
+//     <div id="hello">
+//       HelloWorld - App.js [Fcn] + {props.subtitle}
+//     </div>
+//   );
+// }
+
+// Componentes como Clase:
+class HelloWorld extends React.Component {
+  // Creamos una variable de estado para los botones que se generan en esta clase.
+  state = { show: true };
+  // Método que se activará al presionar botones
+  Button_State = () => {
+    // Uso de las funciones setState de React para cambiar el estado de las variables de esta clase.
+    this.state.show = this.setState({ show: !this.state.show });
+  };
+
+  // Dibujar los componentes que se renderizarán por invocar a esta clase
+  render() {
+    if (this.state.show) {
+      return (
+        <div id="hello">
+          <h3>HelloWorld - App.js [Class] + {this.props.subtitle}</h3>
+          {this.props.my_text}
+          <button onClick={this.Button_State}>Toggle Show</button>
+        </div>
+      );
+    } else
+      return (
+        <div>
+          <h3>none</h3>
+          <button onClick={this.Button_State}>Toggle Show</button>
+        </div>
+      );
+  }
+}
+
+const App2 = () => (
+  <div>
+    Hola App.Js
+    <HelloWorld my_text="Hello Fazt" subtitle="loremp ispum 1" />
+    <HelloWorld my_text="Hola 2" subtitle="loremp ispum 2" />
+    <HelloWorld my_text="Hola 3" subtitle="loremp ispum 3" />
+  </div>
+);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return App2();
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Convertir el elemento <div id="root"> que se encuentra en el "index.html" en una clase App que se encuentra en el archivo "App.js"
+ReactDOM.render(<App />, document.getElementById("root"));
